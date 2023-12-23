fa4e4212333a95b1bd15dd822294e0579c9a8c50
Damian Wasiak <damianwasiak49@gmail.com>
2023-12-23 14:45:04 +0100

    add documentation

--- app/src/main/java/pl/calculator/MainActivity.java
+++ app/src/main/java/pl/calculator/MainActivity.java
@@ -10,99 +10,119 @@ import com.google.android.material.button.MaterialButton;
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Scriptable;
 
+/**
+ * The `MainActivity` class represents the main activity of the calculator application.
+ * It extends the AppCompatActivity class and implements the View.OnClickListener interface.
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    // User interface elements
+    TextView resultTv, solutionTv;
+    MaterialButton buttonC, buttonBrackOpen, buttonBrackClose;
+    MaterialButton buttonDivide, buttonMultiply, buttonPlus, buttonMinus, buttonEquals;
+    MaterialButton button0, button1, button2, button3, button4, button5, button6, button7, button8, button9;
+    MaterialButton buttonAC, buttonDot;
 
-    TextView resultTv,solutionTv;
-    MaterialButton buttonC,buttonBrackOpen,buttonBrackClose;
-    MaterialButton buttonDivide,buttonMultiply,buttonPlus,buttonMinus,buttonEquals;
-    MaterialButton button0,button1,button2,button3,button4,button5,button6,button7,button8,button9;
-    MaterialButton buttonAC,buttonDot;
-
-
+    /**
+     * Method called when the activity is created. Initializes the user interface.
+     *
+     * @param savedInstanceState The saved instance state.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize user interface elements
         resultTv = findViewById(R.id.result_tv);
         solutionTv = findViewById(R.id.solution_tv);
 
-        assignId(buttonC,R.id.button_c);
-        assignId(buttonBrackOpen,R.id.button_open_bracket);
-        assignId(buttonBrackClose,R.id.button_close_bracket);
-        assignId(buttonDivide,R.id.button_divide);
-        assignId(buttonMultiply,R.id.button_multiply);
-        assignId(buttonPlus,R.id.button_plus);
-        assignId(buttonMinus,R.id.button_minus);
-        assignId(buttonEquals,R.id.button_equals);
-        assignId(button0,R.id.button_0);
-        assignId(button1,R.id.button_1);
-        assignId(button2,R.id.button_2);
-        assignId(button3,R.id.button_3);
-        assignId(button4,R.id.button_4);
-        assignId(button5,R.id.button_5);
-        assignId(button6,R.id.button_6);
-        assignId(button7,R.id.button_7);
-        assignId(button8,R.id.button_8);
-        assignId(button9,R.id.button_9);
-        assignId(buttonAC,R.id.button_ac);
-        assignId(buttonDot,R.id.button_dot);
-
-
-
-
-
+        // Assign identifiers and set click listeners for buttons
+        assignId(buttonC, R.id.button_c);
+        assignId(buttonBrackOpen, R.id.button_open_bracket);
+        assignId(buttonBrackClose, R.id.button_close_bracket);
+        assignId(buttonDivide, R.id.button_divide);
+        assignId(buttonMultiply, R.id.button_multiply);
+        assignId(buttonPlus, R.id.button_plus);
+        assignId(buttonMinus, R.id.button_minus);
+        assignId(buttonEquals, R.id.button_equals);
+        assignId(button0, R.id.button_0);
+        assignId(button1, R.id.button_1);
+        assignId(button2, R.id.button_2);
+        assignId(button3, R.id.button_3);
+        assignId(button4, R.id.button_4);
+        assignId(button5, R.id.button_5);
+        assignId(button6, R.id.button_6);
+        assignId(button7, R.id.button_7);
+        assignId(button8, R.id.button_8);
+        assignId(button9, R.id.button_9);
+        assignId(buttonAC, R.id.button_ac);
+        assignId(buttonDot, R.id.button_dot);
     }
 
-    void assignId(MaterialButton btn,int id){
+    /**
+     * Assigns an identifier to a MaterialButton and sets a click listener.
+     *
+     * @param btn The MaterialButton to be assigned.
+     * @param id  The resource identifier for the button.
+     */
+    void assignId(MaterialButton btn, int id) {
         btn = findViewById(id);
         btn.setOnClickListener(this);
     }
 
-
+    /**
+     * Handles button clicks.
+     *
+     * @param view The clicked view (button).
+     */
     @Override
     public void onClick(View view) {
-        MaterialButton button =(MaterialButton) view;
+        MaterialButton button = (MaterialButton) view;
         String buttonText = button.getText().toString();
         String dataToCalculate = solutionTv.getText().toString();
 
-        if(buttonText.equals("AC")){
+        if (buttonText.equals("AC")) {
             solutionTv.setText("");
             resultTv.setText("0");
             return;
         }
-        if(buttonText.equals("=")){
+        if (buttonText.equals("=")) {
             solutionTv.setText(resultTv.getText());
             return;
         }
-        if(buttonText.equals("C")){
-            dataToCalculate = dataToCalculate.substring(0,dataToCalculate.length()-1);
-        }else{
-            dataToCalculate = dataToCalculate+buttonText;
+        if (buttonText.equals("C")) {
+            dataToCalculate = dataToCalculate.substring(0, dataToCalculate.length() - 1);
+        } else {
+            dataToCalculate = dataToCalculate + buttonText;
         }
         solutionTv.setText(dataToCalculate);
 
         String finalResult = getResult(dataToCalculate);
 
-        if(!finalResult.equals("Err")){
+        if (!finalResult.equals("Err")) {
             resultTv.setText(finalResult);
         }
-
     }
 
-    String getResult(String data){
-        try{
-            Context context  = Context.enter();
+    /**
+     * Evaluates the mathematical expression and returns the result as a string.
+     *
+     * @param data The mathematical expression to be evaluated.
+     * @return The result of the evaluation or "Err" if an error occurs.
+     */
+    String getResult(String data) {
+        try {
+            Context context = Context.enter();
             context.setOptimizationLevel(-1);
             Scriptable scriptable = context.initStandardObjects();
-            String finalResult =  context.evaluateString(scriptable,data,"Javascript",1,null).toString();
-            if(finalResult.endsWith(".0")){
-                finalResult = finalResult.replace(".0","");
+            String finalResult = context.evaluateString(scriptable, data, "Javascript", 1, null).toString();
+            if (finalResult.endsWith(".0")) {
+                finalResult = finalResult.replace(".0", "");
             }
             return finalResult;
-        }catch (Exception e){
+        } catch (Exception e) {
             return "Err";
         }
     }
-
 }
